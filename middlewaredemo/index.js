dd2c6df1aa8c43563dc1f7147a09d5b8797f956f
root <root@centos7.linuxvmimages.local>
2022-06-27 13:25:34 -0400

    update mw

--- middlewaredemo/index.js
+++ middlewaredemo/index.js
@@ -1,29 +1,10 @@
 const express = require('express')
+const app = express()
 
-let app = express()
+// 注册路由模块
+const router = require('./router')
+app.use(router)
 
-const middleware1 = function(req,res,next){
-    console.log(`经过中间件函数1，请求的页面${req.path}`)
-    next()
-}
-const middleware2 = function(req,res,next){
-    console.log(`经过中间件函数2，请求的页面${req.path}`)
-    next()
-}
-
-app.use(middleware1)
-app.use(middleware2)
-
-app.get('/',(req,res)=>{
-    
-    res.send('index.html\n')
-})
-app.get('/helloWorld',(req,res)=>{
-    res.send('hello world\n')
-})
-app.get('/others',(req,res)=>{
-    res.send('other pages\n')
-})
-app.listen(80,()=>{
-    console.log('listen to http://127.0.0.1')
+app.listen(80,() => {
+    console.log('http://localhost')
 })
